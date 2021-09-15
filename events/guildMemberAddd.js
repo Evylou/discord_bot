9b7dab90ca1c3c26277e9a5c4a8a57c402b5445a
Evylou <alexandre.meriah@epitech.eu>
2021-09-15 23:38:14 +0200

    ~ guildMemberAdd.js
    Modified so it actually works with data.js.

--- events/guildMemberAddd.js
+++ events/guildMemberAddd.js
@@ -1,7 +1,9 @@
+const on_join_role = require('../data.json').on_join_role
+
 module.exports = {
     name : 'guildMemberAdd',
     once : false,
     execute(guildMember) {
-        guildMember.roles.add(m_data.roles.Guest)
+        guildMember.roles.add(on_join_role)
     }
 }
